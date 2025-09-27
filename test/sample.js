f4bfe14f1718aa608d61659523db9a30427748fe
onurw <80415348+onurw@users.noreply.github.com>
2025-09-27 17:08:38 +0300

    Create sample.js

---
+++ test/sample.js
@@ -0,0 +1,7 @@
+const { expect } = require("chai");
+
+describe("Compilation", function () {
+  it("compiles", async function () {
+    // basit başlangıç testi (deploy sırasında hata almıyorsak yeterli)
+  });
+});
